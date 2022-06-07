3b76545fb936bc6ffa2e1d83b363662b8a1ec851
ChrisMak <cmakride@gmail.com>
2022-06-07 18:34:20 -0400

    Categories Style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
+import './categories.styles.scss' 
+
 const App = () => {
+
+  const categories = [
+    {
+      title: 'Hats',
+      id: 1,
+    },
+    {
+      title: 'Jackets',
+      id: 2,
+    },
+    {
+      title: 'Sneakers',
+      id: 3,
+    },
+    {
+      title: 'Womens',
+      id: 4,
+    },
+    {
+      title: 'Mens',
+      id: 5,
+    }
+  ]
+
+
   return (
-    <div>
-      Hello World
+    <div className="categories-container">
+      {categories.map(({title,id})=>(
+      <div key={id} className="category-container">
+       <div className="background-image"/>
+        <div className="category-body-container">
+          <h2>{title}</h2>
+          <p>shop now</p>
+        </div>
+      </div>
+      ))}
     </div>
   );
 }
